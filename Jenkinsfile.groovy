57cb68cf675d515216d873e8ebac4933e3560933
ERUAM Jehan <eruam32@gmail.com>
2024-11-24 20:02:32 +0100

    test

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    tools {
+        jdk 'JDK23'    // Remplacer par votre version de JDK configurée
+        maven 'Maven3.9.9' // Remplacer par votre version de Maven configurée
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Run') {
+            steps {
+                sh 'mvn exec:java'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline exécuté avec succès!'
+        }
+        failure {
+            echo 'Pipeline échoué!'
+        }
+    }
+}
